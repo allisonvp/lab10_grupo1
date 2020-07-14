e3b71a74d6b93b1ec6737b4df9ece2065596e979
katty <a20160555@pucp.edu.pe>
2020-07-13 21:21:07 -0500

    p2

---
+++ teletok_PostList/index.js
@@ -0,0 +1,56 @@
+const mysql = require('mysql2');
+
+exports.handler = (event, context, callback) => {
+
+    var conn = mysql.createConnection({
+        host: "database-pruebas.cvdiq9drvcm2.us-east-1.rds.amazonaws.com",
+        user: "admin",
+        password: "bvdm9iSLzQWT2YutZmw5",
+        port: 3306,
+        database: "teletok_lambda"
+    });
+
+    conn.connect(function(error) {
+        if (error) {
+            conn.end(function() {
+                callback(error, {
+                    statusCode: 400,
+                    body: JSON.stringify({
+                        "estado": "error",
+                        "msg": error
+                    }),
+                });
+            });
+        }
+        else {
+            conn.query("SELECT p.id, p.description, p.creation_date, u.username, count(c.post_id) as 'commentCount' FROM teletok_lambda.post p INNER JOIN post_comment c ON (c.post_id = p.id) INNER JOIN user u ON (u.id = p.id) GROUP BY p.id", 
+            function(err, result) {
+                if (err) {
+                    conn.end(function(){;
+                    callback(err, {
+                        statusCode: 400,
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify({
+                            "estado": "error",
+                            "msg": err
+                        })
+                    });
+                    });
+                }
+                else {
+                    conn.end(function() {
+                        callback(null,  {
+                            statusCode: 200,
+                            headers: { 'Content-Type': 'application/json' },
+                            body: JSON.stringify({
+                                "estado": "ok",
+                                "msg": result
+                            })
+                        });
+
+                    });
+                }
+            });
+        }
+    });
+};
